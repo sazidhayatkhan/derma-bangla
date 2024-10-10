cba99814671a36c8d06f226e05d2d2fc446c8bf4
sazidhayatkhan <sazidhayatkhan@gmail.com>
2024-10-10 22:35:34 +0600

    fixed issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images : {
+        domains : ["scontent.fdac14-1.fna.fbcdn.net"]
+    }
+};
 
 export default nextConfig;
